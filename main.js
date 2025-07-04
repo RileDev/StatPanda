63bfd8a74848659379912aeb55ebbacd9665fde0
RileDev <71150306+RileDev@users.noreply.github.com>
2025-07-04 20:11:12 +0200

    uses data from user

--- main.js
+++ main.js
@@ -15,16 +15,15 @@ addBtn.addEventListener("click", () => {
 })
 
 generateBtn.addEventListener("click", () => {
-    const data = [
-        1, 10, 7, 16, 8, 4, 12, 5, 2, 2,
-        5, 7, 9, 10, 13, 15, 18, 20, 1, 22,
-        30, 35, 4, 8, 6, 3, 15, 17, 29, 36,
-        3, 9, 15, 6, 7, 38, 13, 10, 12, 15,
-        14, 18, 10, 5, 3, 2, 1, 21, 19, 11
-    ];
+    // const data = [
+    //     1, 10, 7, 16, 8, 4, 12, 5, 2, 2,
+    //     5, 7, 9, 10, 13, 15, 18, 20, 1, 22,
+    //     30, 35, 4, 8, 6, 3, 15, 17, 29, 36,
+    //     3, 9, 15, 6, 7, 38, 13, 10, 12, 15,
+    //     14, 18, 10, 5, 3, 2, 1, 21, 19, 11
+    // ];
 
-    // distribution = new FrequencyDistribution(fetchData(), intervalCb.checked);
-    distribution = new FrequencyDistribution(data, intervalCb.checked);
+    distribution = new FrequencyDistribution(fetchData(), intervalCb.checked);    
     displayTable(distribution.json);
 
     
